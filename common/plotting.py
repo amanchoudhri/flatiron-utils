d55781640b2276be3d58a1b04ae19b8839223165
amanchoudhri <amanchoudhri@gmail.com>
2022-07-08 09:45:25 -0400

    Add type hints and a fn to plot calibration curve

--- common/plotting.py
+++ common/plotting.py
@@ -1,21 +1,47 @@
 import math
-from matplotlib import pyplot as plt
 
-def subplots(n_plots, sharex=True, sharey=True, **kwargs):
+from typing import List, Tuple
+
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+def subplots(n_plots, sharex=True, sharey=True, **kwargs) -> Tuple[Figure, List[Axes]]:
     """
     Create nicely sized and laid-out subplots for a desired number of plots.
     """
     # essentially we want to make the subplots as square as possible
     # number of rows is the largest factor of n_plots less than sqrt(n_plots)
-    options = range(1, int(math.sqrt(n_plots)))
+    options = range(1, int(math.sqrt(n_plots) + 1))
     n_rows = max(filter(lambda n: n_plots % n == 0, options))
     n_cols = int(n_plots / n_rows)
     # now generate the Figure and Axes pyplot objects
-    SCALE_FACTOR = 3  # cosmetic scale factor to make larger plot
+    SCALE_FACTOR = 4  # cosmetic scale factor to make larger plot
     figsize = (n_cols * SCALE_FACTOR, n_rows * SCALE_FACTOR)
     fig, axs = plt.subplots(
         n_rows, n_cols, figsize=figsize,
         sharex=sharex, sharey=sharey,
         **kwargs
         )
-    return fig, axs
+    flattened_axes = [ax for ax_row in axs for ax in ax_row]
+    return fig, flattened_axes
+
+def plot_calibration_curve(
+    bin_edges,
+    true_props,
+    ax = None
+    ) -> Tuple[np.ndarray, np.ndarray, Axes]:
+    """
+    Plot the given calibration curve, returning the x and y values
+    along with the axis on which the curve was plotted.
+    """
+    if not ax:
+        _, ax = plt.subplots()
+    ax.scatter(bin_edges, true_props)
+    ax.plot([0, 1], [0, 1], color='grey', linestyle='dashed')
+    ax.set_xlabel('Probability assigned to region around mode')
+    ax.set_ylabel('True proportion of samples in region')
+    return bin_edges, true_props, ax
